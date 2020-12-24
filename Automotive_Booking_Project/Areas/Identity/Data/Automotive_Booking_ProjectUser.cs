b40c677fd79fa669806342988d7b1e5e228c7669
Luke <LP030@atcltd.atc.org.nz>
2020-12-25 00:20:37 +1300

    Add project files.

---
+++ Automotive_Booking_Project/Areas/Identity/Data/Automotive_Booking_ProjectUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Automotive_Booking_Project.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Automotive_Booking_ProjectUser class
+    public class Automotive_Booking_ProjectUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Firstname { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Automotive_Booking_Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Automotive_Booking_Project.Areas.Identity.Data;
+using Automotive_Booking_Project.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Automotive_Booking_Project.Areas.Identity.IdentityHostingStartup))]
+namespace Automotive_Booking_Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Automotive_Booking_IdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Automotive_Booking_IdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<Automotive_Booking_ProjectUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Automotive_Booking_IdentityDbContext>();
+            });
+        }
+    }
+}
