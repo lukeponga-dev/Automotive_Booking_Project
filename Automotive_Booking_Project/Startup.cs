91f01013528409e08b485faf529109f5362af5ae
Luke <LP030@atcltd.atc.org.nz>
2021-03-04 12:37:54 +1300

    update

--- Automotive_Booking_Project/Startup.cs
+++ Automotive_Booking_Project/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Automotive_Booking_Project.Data;
 
 namespace Automotive_Booking_Project
 {
@@ -25,6 +27,8 @@ namespace Automotive_Booking_Project
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+    
 
         }
 
