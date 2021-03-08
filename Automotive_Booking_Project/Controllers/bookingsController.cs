c1c880324260f7154fdcd43480b176550b9f89aa
Luke <LP030@atcltd.atc.org.nz>
2021-03-08 16:18:32 +1300

    idk

---
+++ Automotive_Booking_Project/Controllers/bookingsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Automotive_Booking_Project.Data;
+using Automotive_Booking_Project.Models;
+
+namespace Automotive_Booking_Project.Controllers
+{
+    public class bookingsController : Controller
+    {
+        private readonly Automotive_Booking_ProjectContext _context;
+
+        public bookingsController(Automotive_Booking_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: bookings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.bookings.ToListAsync());
+        }
+
+        // GET: bookings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.bookings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookings);
+        }
+
+        // GET: bookings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] bookings bookings)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookings);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookings);
+        }
+
+        // GET: bookings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.bookings.FindAsync(id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+            return View(bookings);
+        }
+
+        // POST: bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] bookings bookings)
+        {
+            if (id != bookings.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookings);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!bookingsExists(bookings.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookings);
+        }
+
+        // GET: bookings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.bookings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookings);
+        }
+
+        // POST: bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bookings = await _context.bookings.FindAsync(id);
+            _context.bookings.Remove(bookings);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool bookingsExists(int id)
+        {
+            return _context.bookings.Any(e => e.Id == id);
+        }
+    }
+}
--- Automotive_Booking_Project/Data/Automotive_Booking_ProjectContext.cs
+++ Automotive_Booking_Project/Data/Automotive_Booking_ProjectContext.cs
@@ -15,5 +15,7 @@ namespace Automotive_Booking_Project.Data
         }
 
         public DbSet<Automotive_Booking_Project.Models.Users> Users { get; set; }
+
+        public DbSet<Automotive_Booking_Project.Models.bookings> bookings { get; set; }
     }
 }
